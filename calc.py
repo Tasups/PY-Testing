3cd37df42c1c920d388a9043396ce3a5ac394a03
Jason Whisnant <tasups@gmail.com>
2022-11-16 21:22:46 +0000

    edited author

---
+++ calc.py
@@ -0,0 +1,18 @@
+def add(x,y):
+  """write function"""
+  return x + y
+  
+def subtract(x,y):
+  """write function"""
+  return x - y
+  
+def multiply(x,y):
+  """write function"""
+  return x * y
+  
+def divide(x,y):
+  """write function"""
+  if(y == 0):
+    raise ValueError("cannot divide by zero!")
+  return x / y
+  
