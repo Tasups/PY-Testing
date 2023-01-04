26781fac55ebe9cc9a6e7c83169a4926fa2527d5
Jason Whisnant <tasups@gmail.com>
2023-01-04 16:18:10 +0000

    input function finished

---
+++ PythonTutorial/pythonInputFunction.py
@@ -0,0 +1,9 @@
+# input("What is your name, bruh?")
+
+# print("Wassup, " + input("What is your name, bruh?") + "?")
+
+name = input("Please enter your name: ")
+nameLength = str(len(name))
+
+
+print("The length of your name is:" + " " + nameLength)
