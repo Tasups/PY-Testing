9b9cc9fbf5a0cfe746e9d9d8acf6bf2d4e657d15
Jason Whisnant <tasups@gmail.com>
2023-01-10 01:02:59 +0000

    coin flip added

---
+++ PythonTutorial/4day/random_examples.py
@@ -0,0 +1,13 @@
+import random
+
+# random from 1 to 10
+random_integer = random.randint(1,10)
+print(random_integer)
+
+# random from 0.0 to 1 but not including 1
+random_float = random.random()
+print(random_float)
+
+# random number from 0 to 5
+ran_0to5 = random_float * 5
+print(ran_0to5)
