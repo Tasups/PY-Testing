973eaf3660ff447193a36f39d469d06a4f712666
Jason Whisnant <tasups@gmail.com>
2023-01-31 21:34:36 +0000

    caesar cipher done

--- PythonTutorial/8day/caesar_cypher.py
+++ PythonTutorial/8day/caesar_cypher.py
@@ -1,16 +1,5 @@
 # Caesar Cypher app
 
-# efficient code
-
-import sys
-
-
-print("THE CAESAR CYPHER ENCODER/DECODER")
-direction = input('Type "encode" to encrypt or "decode" to decrypt a message.\n')
-text =  input('Type your message:\n').lower()
-shift = int(input('Type the number to shift:\n'))
-
-  
 def caesar(start_text, shift_amount, cipher_direction):
   # We could use an error to show that the shift amount must be 26 or less or we could
   # if shift_amount > 26:
@@ -18,16 +7,15 @@ def caesar(start_text, shift_amount, cipher_direction):
   #   sys.exit()
       
   # change the shift to this
-  shift_amount = shift_amount % 26
       
   alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   end_text = ""
   if cipher_direction == "decode":
     shift_amount *= -1
-  for letter in start_text:
-    in_alphabet = alphabet.count(letter)
+  for char in start_text:
+    in_alphabet = alphabet.count(char)
     if in_alphabet > 0:
-      position = alphabet.index(letter)
+      position = alphabet.index(char)
       new_position = position + shift_amount
       if new_position > 26:
         new_position -= 26
@@ -35,11 +23,23 @@ def caesar(start_text, shift_amount, cipher_direction):
         new_position += 26
       end_text += alphabet[new_position]
     if in_alphabet <= 0:
-      end_text += letter
+      end_text += char
   print(f"The {cipher_direction}d text is {end_text}")
-
-
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  
+    
+should_continue = True
+    
+while should_continue:
+  print("THE CAESAR CYPHER ENCODER/DECODER")
+  direction = input('Type "encode" to encrypt or "decode" to decrypt a message.\n')
+  text =  input('Type your message:\n').lower()
+  shift = int(input('Type the number to shift:\n'))
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  go_again = input("Would you like to run the program again? Type 'yes' or 'no'.\n")
+  if go_again == "no":
+    print("Goodbye")
+    should_continue = False
 
 # This is how I did the program, which works.  
 
